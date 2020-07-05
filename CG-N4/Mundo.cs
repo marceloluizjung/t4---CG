85ab8e301f791f4aaf7208da4ca80b5321a4ac44
Marcelo <marcelo_4940@hotmail.com>
2020-07-05 00:37:19 -0300

    Cenário

--- CG-N4/Mundo.cs
+++ CG-N4/Mundo.cs
@@ -27,222 +27,261 @@ using CG_Biblioteca;
 //TODO: Unproject
 namespace gcgcg
 {
-  class Mundo : GameWindow
-  {
-    private static Mundo instanciaMundo = null;
-
-    private Mundo(int width, int height) : base(width, height) { }
-
-    public static Mundo GetInstance(int width, int height)
+    class Mundo : GameWindow
     {
-      if (instanciaMundo == null)
-        instanciaMundo = new Mundo(width, height);
-      return instanciaMundo;
-    }
+        private static Mundo instanciaMundo = null;
 
-    private CameraPerspective camera = new CameraPerspective();
-    protected List<Objeto> objetosLista = new List<Objeto>();
-    private ObjetoGeometria objetoSelecionado = null;
-    private bool bBoxDesenhar = false;
-    int mouseX, mouseY;   //TODO: achar método MouseDown para não ter variável Global
-    private Poligono objetoNovo = null;
-    private String objetoId = "A";
-    private Retangulo obj_Retangulo;
-    private Cubo obj_Cubo;
-    private Cilindro obj_Cilindro;
-    private Esfera obj_Esfera;
-    private Cone obj_Cone;
+        private Mundo(int width, int height) : base(width, height) { }
 
-    protected override void OnLoad(EventArgs e)
-    {
-      base.OnLoad(e);
+        public static Mundo GetInstance(int width, int height)
+        {
+            if (instanciaMundo == null)
+                instanciaMundo = new Mundo(width, height);
+            return instanciaMundo;
+        }
 
-      Console.WriteLine(" --- Ajuda / Teclas: ");
-      Console.WriteLine(" [  H     ] mostra teclas usadas. ");
+        private CameraPerspective camera = new CameraPerspective();
+        protected List<Objeto> objetosLista = new List<Objeto>();
+        private ObjetoGeometria objetoSelecionado = null;
+        private bool bBoxDesenhar = false;
+        int mouseX, mouseY;   //TODO: achar método MouseDown para não ter variável Global
+        private Poligono objetoNovo = null;
+        private String objetoId = "A";
+        private Retangulo obj_Retangulo;
+        private Cubo obj_Cubo;
+        private Cilindro obj_Cilindro;
+        private Esfera obj_Esfera;
+        private Cone obj_Cone;
 
-      obj_Cubo = new Cubo("Chao", null);
-      objetosLista.Add(obj_Cubo);
-      obj_Cubo.EscalaXYZ(7, 1, 5);
-      obj_Cubo.TranslacaoXYZ(3, -1, 2);
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
 
-      obj_Cubo = new Cubo("Barreira1", null);
-      objetosLista.Add(obj_Cubo);
-      obj_Cubo.TranslacaoXYZ(0, 0, 0);
+            Console.WriteLine(" --- Ajuda / Teclas: ");
+            Console.WriteLine(" [  H     ] mostra teclas usadas. ");
 
-      obj_Cubo = new Cubo("Barreira2", null);
-      objetosLista.Add(obj_Cubo);
-      obj_Cubo.TranslacaoXYZ(0, 0, 1);
+            //Chão
+            obj_Cubo = new Cubo("Chao", null);
+            objetosLista.Add(obj_Cubo);
+            obj_Cubo.EscalaXYZ(1000, 1, 1000);
+            obj_Cubo.TranslacaoXYZ(3, -1, 2);
 
-      obj_Cubo = new Cubo("Personagem", null);
-      objetosLista.Add(obj_Cubo);
-      obj_Cubo.TranslacaoXYZ(1, 0, 3);
+            //Cerca
+            obj_Cubo = new Cubo("Cerca1", null);
+            objetosLista.Add(obj_Cubo);
+            obj_Cubo.EscalaXYZ(30, 100, 1000);
+            obj_Cubo.TranslacaoXYZ(485, 55, 0);
 
-      objetoSelecionado = obj_Cubo;
+            obj_Cubo = new Cubo("Cerca2", null);
+            objetosLista.Add(obj_Cubo);
+            obj_Cubo.EscalaXYZ(1000, 100, 30);
+            obj_Cubo.TranslacaoXYZ(0, 55, -485);
 
-      camera.Eye = new Vector3(3.5f, 8, 22);
-      camera.At = new Vector3(3.5f, 0.5f, 2.5f);
-      camera.Near = 0.1f;
-      camera.Far = 60.0f;
+            obj_Cubo = new Cubo("Cerca3", null);
+            objetosLista.Add(obj_Cubo);
+            obj_Cubo.EscalaXYZ(30, 100, 1000);
+            obj_Cubo.TranslacaoXYZ(-485, 55, 0);
 
-      GL.ClearColor(127,127,127,255);
-      GL.Enable(EnableCap.DepthTest);
-      GL.Enable(EnableCap.CullFace);
-      // GL.Disable(EnableCap.CullFace);
-    }
-    protected override void OnResize(EventArgs e)
-    {
-      base.OnResize(e);
+            obj_Cubo = new Cubo("Cerca4", null);
+            objetosLista.Add(obj_Cubo);
+            obj_Cubo.EscalaXYZ(1000, 100, 30);
+            obj_Cubo.TranslacaoXYZ(0, 55, 485);
 
-      GL.Viewport(ClientRectangle.X, ClientRectangle.Y, ClientRectangle.Width, ClientRectangle.Height);
+            //Barreira
+            obj_Cubo = new Cubo("Barreira1", null);
+            objetosLista.Add(obj_Cubo);
+            obj_Cubo.EscalaXYZ(100, 100, 100);
+            obj_Cubo.TranslacaoXYZ(420, 55, -298);
 
-      Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView(camera.Fovy, Width / (float)Height, camera.Near, camera.Far);
-      GL.MatrixMode(MatrixMode.Projection);
-      GL.LoadMatrix(ref projection);
-    }
+            obj_Cubo = new Cubo("Barreira2", null);
+            objetosLista.Add(obj_Cubo);
+            obj_Cubo.EscalaXYZ(100, 100, 100);
+            obj_Cubo.TranslacaoXYZ(-420, 55, 298);
 
-    protected override void OnUpdateFrame(FrameEventArgs e)
-    {
-      base.OnUpdateFrame(e);
-    }
-    protected override void OnRenderFrame(FrameEventArgs e)
-    {
-      base.OnRenderFrame(e);
-      GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-      Matrix4 modelview = Matrix4.LookAt(camera.Eye, camera.At, camera.Up);
-      GL.MatrixMode(MatrixMode.Modelview);
-      GL.LoadMatrix(ref modelview);
-#if CG_Gizmo      
-      Sru3D();
+            obj_Cubo = new Cubo("Barreira3", null);
+            objetosLista.Add(obj_Cubo);
+            obj_Cubo.EscalaXYZ(650, 100, 100);
+            obj_Cubo.TranslacaoXYZ(0, 55, 0);
+
+            //Personagens
+                //Inimigos
+                obj_Cubo = new Cubo("Inimigo01", null);
+                objetosLista.Add(obj_Cubo);
+                obj_Cubo.EscalaXYZ(100, 100, 100);
+                obj_Cubo.TranslacaoXYZ(0, 55, -350);
+
+                //Eu
+                obj_Cubo = new Cubo("Personagem", null);
+                objetosLista.Add(obj_Cubo);
+                obj_Cubo.EscalaXYZ(100, 100, 100);
+                obj_Cubo.TranslacaoXYZ(0, 55, 350);
+
+            objetoSelecionado = obj_Cubo;
+
+            camera.Eye = new Vector3(0f, 1400, 850);
+            camera.At = new Vector3(0f, 0f, 0f);
+            camera.Near = 100f;
+            camera.Far = 2000.0f;
+
+            GL.ClearColor(0, 0, 0, 255);
+            GL.Enable(EnableCap.DepthTest);
+            GL.Enable(EnableCap.CullFace);
+            // GL.Disable(EnableCap.CullFace);
+        }
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            GL.Viewport(ClientRectangle.X, ClientRectangle.Y, ClientRectangle.Width, ClientRectangle.Height);
+
+            Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView(camera.Fovy, Width / (float)Height, camera.Near, camera.Far);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref projection);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+        }
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            Matrix4 modelview = Matrix4.LookAt(camera.Eye, camera.At, camera.Up);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref modelview);
+#if CG_Gizmo
+            Sru3D();
 #endif
-      for (var i = 0; i < objetosLista.Count; i++)
-        objetosLista[i].Desenhar();
-      if (bBoxDesenhar && (objetoSelecionado != null))
-        objetoSelecionado.BBox.Desenhar();
-      this.SwapBuffers();
-    }
+            for (var i = 0; i < objetosLista.Count; i++)
+                objetosLista[i].Desenhar();
+            if (bBoxDesenhar && (objetoSelecionado != null))
+                objetoSelecionado.BBox.Desenhar();
+            this.SwapBuffers();
+        }
 
-    protected override void OnKeyDown(OpenTK.Input.KeyboardKeyEventArgs e)
-    {
-      if (e.Key == Key.H)
-        Utilitario.AjudaTeclado();
-      else if (e.Key == Key.Escape)
-        Exit();
-      else if (e.Key == Key.E)
-      {
-        Console.WriteLine("--- Objetos / Pontos: ");
-        for (var i = 0; i < objetosLista.Count; i++)
+        protected override void OnKeyDown(OpenTK.Input.KeyboardKeyEventArgs e)
         {
-          Console.WriteLine(objetosLista[i]);
+            if (e.Key == Key.H)
+                Utilitario.AjudaTeclado();
+            else if (e.Key == Key.Escape)
+                Exit();
+            else if (e.Key == Key.E)
+            {
+                Console.WriteLine("--- Objetos / Pontos: ");
+                for (var i = 0; i < objetosLista.Count; i++)
+                {
+                    Console.WriteLine(objetosLista[i]);
+                }
+            }
+            else if (e.Key == Key.O)
+                bBoxDesenhar = !bBoxDesenhar;
+            else if (e.Key == Key.Enter)
+            {
+                if (objetoNovo != null)
+                {
+                    objetoNovo.PontosRemoverUltimo();   // N3-Exe6: "truque" para deixar o rastro
+                    objetoSelecionado = objetoNovo;
+                    objetoNovo = null;
+                }
+            }
+            else if (e.Key == Key.Space)
+            {
+                if (objetoNovo == null)
+                {
+                    objetoNovo = new Poligono(objetoId + 1, null);
+                    objetosLista.Add(objetoNovo);
+                    objetoNovo.PontosAdicionar(new Ponto4D(mouseX, mouseY));
+                    objetoNovo.PontosAdicionar(new Ponto4D(mouseX, mouseY));  // N3-Exe6: "troque" para deixar o rastro
+                }
+                else
+                    objetoNovo.PontosAdicionar(new Ponto4D(mouseX, mouseY));
+            }
+            else if (objetoSelecionado != null)
+            {
+                if (e.Key == Key.M)
+                    Console.WriteLine(objetoSelecionado.Matriz);
+                else if (e.Key == Key.P)
+                    Console.WriteLine(objetoSelecionado);
+                else if (e.Key == Key.I)
+                    objetoSelecionado.AtribuirIdentidade();
+                //TODO: não está atualizando a BBox com as transformações geométricas
+                else if (e.Key == Key.Left)
+                    objetoSelecionado.TranslacaoXYZ(-10, 0, 0);
+                else if (e.Key == Key.Right)
+                    objetoSelecionado.TranslacaoXYZ(10, 0, 0);
+                else if (e.Key == Key.Up)
+                    objetoSelecionado.TranslacaoXYZ(0, 0, -10);
+                else if (e.Key == Key.Down)
+                    objetoSelecionado.TranslacaoXYZ(0, 0, 10);
+                else if (e.Key == Key.Number8)
+                    objetoSelecionado.TranslacaoXYZ(0, 0, 10);
+                else if (e.Key == Key.Number9)
+                    objetoSelecionado.TranslacaoXYZ(0, 0, -10);
+                else if (e.Key == Key.PageUp)
+                    objetoSelecionado.EscalaXYZ(2, 2, 2);
+                else if (e.Key == Key.PageDown)
+                    objetoSelecionado.EscalaXYZ(0.5, 0.5, 0.5);
+                else if (e.Key == Key.Home)
+                    objetoSelecionado.EscalaXYZBBox(0.5, 0.5, 0.5);
+                else if (e.Key == Key.End)
+                    objetoSelecionado.EscalaXYZBBox(2, 2, 2);
+                else if (e.Key == Key.Number1)
+                    objetoSelecionado.Rotacao(10);
+                else if (e.Key == Key.Number2)
+                    objetoSelecionado.Rotacao(-10);
+                else if (e.Key == Key.Number3)
+                    objetoSelecionado.RotacaoZBBox(10);
+                else if (e.Key == Key.Number4)
+                    objetoSelecionado.RotacaoZBBox(-10);
+                else if (e.Key == Key.Number0)
+                    objetoSelecionado = null;
+                else if (e.Key == Key.X)
+                    objetoSelecionado.TrocaEixoRotacao('x');
+                else if (e.Key == Key.Y)
+                    objetoSelecionado.TrocaEixoRotacao('y');
+                else if (e.Key == Key.Z)
+                    objetoSelecionado.TrocaEixoRotacao('z');
+                else
+                    Console.WriteLine(" __ Tecla não implementada.");
+            }
+            else
+                Console.WriteLine(" __ Tecla não implementada.");
         }
-      }
-      else if (e.Key == Key.O)
-        bBoxDesenhar = !bBoxDesenhar;
-      else if (e.Key == Key.Enter)
-      {
-        if (objetoNovo != null)
-        {
-          objetoNovo.PontosRemoverUltimo();   // N3-Exe6: "truque" para deixar o rastro
-          objetoSelecionado = objetoNovo;
-          objetoNovo = null;
-        }
-      }
-      else if (e.Key == Key.Space)
-      {
-        if (objetoNovo == null)
-        {
-          objetoNovo = new Poligono(objetoId + 1, null);
-          objetosLista.Add(objetoNovo);
-          objetoNovo.PontosAdicionar(new Ponto4D(mouseX, mouseY));
-          objetoNovo.PontosAdicionar(new Ponto4D(mouseX, mouseY));  // N3-Exe6: "troque" para deixar o rastro
-        }
-        else
-          objetoNovo.PontosAdicionar(new Ponto4D(mouseX, mouseY));
-      }
-      else if (objetoSelecionado != null)
-      {
-        if (e.Key == Key.M)
-          Console.WriteLine(objetoSelecionado.Matriz);
-        else if (e.Key == Key.P)
-          Console.WriteLine(objetoSelecionado);
-        else if (e.Key == Key.I)
-          objetoSelecionado.AtribuirIdentidade();
-        //TODO: não está atualizando a BBox com as transformações geométricas
-        else if (e.Key == Key.Left)
-          objetoSelecionado.TranslacaoXYZ(-0.1, 0, 0);
-        else if (e.Key == Key.Right)
-          objetoSelecionado.TranslacaoXYZ(0.1, 0, 0);
-        else if (e.Key == Key.Up)
-          objetoSelecionado.TranslacaoXYZ(0, 0, -0.1);
-        else if (e.Key == Key.Down)
-          objetoSelecionado.TranslacaoXYZ(0, 0, 0.1);
-        else if (e.Key == Key.Number8)
-          objetoSelecionado.TranslacaoXYZ(0, 0, 10);
-        else if (e.Key == Key.Number9)
-          objetoSelecionado.TranslacaoXYZ(0, 0, -10);
-        else if (e.Key == Key.PageUp)
-          objetoSelecionado.EscalaXYZ(2, 2, 2);
-        else if (e.Key == Key.PageDown)
-          objetoSelecionado.EscalaXYZ(0.5, 0.5, 0.5);
-        else if (e.Key == Key.Home)
-          objetoSelecionado.EscalaXYZBBox(0.5, 0.5, 0.5);
-        else if (e.Key == Key.End)
-          objetoSelecionado.EscalaXYZBBox(2, 2, 2);
-        else if (e.Key == Key.Number1)
-          objetoSelecionado.Rotacao(10);
-        else if (e.Key == Key.Number2)
-          objetoSelecionado.Rotacao(-10);
-        else if (e.Key == Key.Number3)
-          objetoSelecionado.RotacaoZBBox(10);
-        else if (e.Key == Key.Number4)
-          objetoSelecionado.RotacaoZBBox(-10);
-        else if (e.Key == Key.Number0)
-          objetoSelecionado = null;
-        else if (e.Key == Key.X)
-          objetoSelecionado.TrocaEixoRotacao('x');
-        else if (e.Key == Key.Y)
-          objetoSelecionado.TrocaEixoRotacao('y');
-        else if (e.Key == Key.Z)
-          objetoSelecionado.TrocaEixoRotacao('z');
-        else
-          Console.WriteLine(" __ Tecla não implementada.");
-      }
-      else
-        Console.WriteLine(" __ Tecla não implementada.");
-    }
 
-    //TODO: não está considerando o NDC
-    protected override void OnMouseMove(MouseMoveEventArgs e)
-    {
-      mouseX = e.Position.X; mouseY = 600 - e.Position.Y; // Inverti eixo Y
-      if (objetoNovo != null)
-      {
-        objetoNovo.PontosUltimo().X = mouseX;
-        objetoNovo.PontosUltimo().Y = mouseY;
-      }
-    }
+        //TODO: não está considerando o NDC
+        protected override void OnMouseMove(MouseMoveEventArgs e)
+        {
+            mouseX = e.Position.X; mouseY = 600 - e.Position.Y; // Inverti eixo Y
+            if (objetoNovo != null)
+            {
+                objetoNovo.PontosUltimo().X = mouseX;
+                objetoNovo.PontosUltimo().Y = mouseY;
+            }
+        }
 
 #if CG_Gizmo
-    private void Sru3D()
-    {
-      GL.LineWidth(1);
-      GL.Begin(PrimitiveType.Lines);
-      GL.Color3(OpenTK.Color.Red);
-      GL.Vertex3(0, 0, 0); GL.Vertex3(200, 0, 0);
-      GL.Color3(OpenTK.Color.Green);
-      GL.Vertex3(0, 0, 0); GL.Vertex3(0, 200, 0);
-      GL.Color3(OpenTK.Color.Blue);
-      GL.Vertex3(0, 0, 0); GL.Vertex3(0, 0, 200);
-      GL.End();
+        private void Sru3D()
+        {
+            GL.LineWidth(1);
+            GL.Begin(PrimitiveType.Lines);
+            GL.Color3(OpenTK.Color.Red);
+            GL.Vertex3(0, 0, 0); GL.Vertex3(200, 0, 0);
+            GL.Color3(OpenTK.Color.Green);
+            GL.Vertex3(0, 0, 0); GL.Vertex3(0, 200, 0);
+            GL.Color3(OpenTK.Color.Blue);
+            GL.Vertex3(0, 0, 0); GL.Vertex3(0, 0, 200);
+            GL.End();
+        }
+#endif
     }
-#endif    
-  }
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Mundo window = Mundo.GetInstance(600, 600);
-      window.Title = "CG-N4";
-      window.Run(1.0 / 60.0);
+        static void Main(string[] args)
+        {
+            Mundo window = Mundo.GetInstance(600, 600);
+            window.Title = "CG-N4";
+            window.Run(1.0 / 60.0);
+        }
     }
-  }
 }
